2e09cbb68e8341fbcd10b195c0375abba30d090d
anantkrsingh <anantkrplay@gmail.com>
2024-09-30 10:50:13 +0530

    Changed firebase & added analytics

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,173 +1,173 @@
-import { createSocket } from './socket.js'
+import { createSocket } from "./socket.js";
 
-const $ = (x) => document.querySelector(x)
+const $ = (x) => document.querySelector(x);
 const esc = (x) => {
-  const txt = document.createTextNode(x)
-  const p = document.createElement('p')
-  p.appendChild(txt)
-  return p.innerHTML
-}
+  const txt = document.createTextNode(x);
+  const p = document.createElement("p");
+  p.appendChild(txt);
+  return p.innerHTML;
+};
 
-const ws = await createSocket()
-const debounceTime = 1000
-let base = 0
-const noise = 0
+const ws = await createSocket();
+const debounceTime = 1000;
+let base = 0;
+const noise = 0;
 
-if (!sessionStorage.getItem('peopleOnline')) {
-  sessionStorage.setItem('peopleOnline', base)
+if (!sessionStorage.getItem("peopleOnline")) {
+  sessionStorage.setItem("peopleOnline", base);
 } else {
-  base = +sessionStorage.getItem('peopleOnline')
+  base = +sessionStorage.getItem("peopleOnline");
 }
 
-let timeout
+let timeout;
 
-const $peopleOnline = $('#peopleOnline p span')
-const $skipBtn = $('#skip-btn')
-const $sendBtn = $('#send-btn')
-const $msgs = $('#messages')
-const $msgArea = $('#message-area')
-const $typing = $('#typing')
-const $input = $('#message-input')
+const $peopleOnline = $("#peopleOnline p span");
+const $skipBtn = $("#skip-btn");
+const $sendBtn = $("#send-btn");
+const $msgs = $("#messages");
+const $msgArea = $("#message-area");
+const $typing = $("#typing");
+const $input = $("#message-input");
 
 function configureChat() {
-  $input.focus()
+  $input.focus();
 
-  document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape') {
-      $skipBtn.click()
-      e.preventDefault()
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+      $skipBtn.click();
+      e.preventDefault();
     }
-  })
-  $input.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-      clearInterval(timeout)
-      ws.emit('typing', false)
-      $sendBtn.click()
-      return e.preventDefault()
+  });
+  $input.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      clearInterval(timeout);
+      ws.emit("typing", false);
+      $sendBtn.click();
+      return e.preventDefault();
     }
-    ws.emit('typing', true)
-  })
-  $input.addEventListener('keyup', function (e) {
-    clearInterval(timeout)
+    ws.emit("typing", true);
+  });
+  $input.addEventListener("keyup", function (e) {
+    clearInterval(timeout);
     timeout = setTimeout(() => {
-      ws.emit('typing', false)
-    }, debounceTime)
-  })
+      ws.emit("typing", false);
+    }, debounceTime);
+  });
 }
 
 const initializeConnection = () => {
   $msgs.innerHTML = `
     <div class="message-status">Looking for someone you can chat with...</div>
-  `
-  $sendBtn.disabled = true
-  $input.value = ''
-  $input.readOnly = true
+  `;
+  $sendBtn.disabled = true;
+  $input.value = "";
+  $input.readOnly = true;
 
-  ws.emit('peopleOnline')
-  const params = new URLSearchParams(window.location.search)
+  ws.emit("peopleOnline");
+  const params = new URLSearchParams(window.location.search);
   const interests =
     params
-      .get('interests')
-      ?.split(',')
+      .get("interests")
+      ?.split(",")
       .filter((x) => !!x)
-      .map((x) => x.trim()) || []
-  ws.emit('match', { data: 'text', interests })
-}
+      .map((x) => x.trim()) || [];
+  ws.emit("match", { data: "text", interests });
+};
 
 const disconnectConnection = () => {
   $msgs.innerHTML = `
     <div class="message-status">Stranger Has Disconnected ...</div>
-  `
-  $typing.style.display =  'none'
-  $sendBtn.disabled = true
-  $input.value = ''
-  $input.readOnly = true
+  `;
+  $typing.style.display = "none";
+  $sendBtn.disabled = true;
+  $input.value = "";
+  $input.readOnly = true;
 
-  ws.emit('peopleOnline')
-}
+  ws.emit("peopleOnline");
+};
 
-$skipBtn.addEventListener('click', async () => {
-  ws.emit('disconnect')
-  initializeConnection()
-})
+$skipBtn.addEventListener("click", async () => {
+  ws.emit("disconnect");
+  initializeConnection();
+});
 
-$sendBtn.addEventListener('click', () => {
-  const msg = $input.value.trim()
-  if (!msg) return
+$sendBtn.addEventListener("click", () => {
+  const msg = $input.value.trim();
+  if (!msg) return;
 
-  const msgE = document.createElement('div')
-  msgE.className = 'message'
-  msgE.innerHTML = `<span class="you">You:</span> ${esc(msg)}`
+  const msgE = document.createElement("div");
+  msgE.className = "message";
+  msgE.innerHTML = `<span class="you">You:</span> ${esc(msg)}`;
 
-  $msgs.appendChild(msgE)
-  $msgArea.scrollTop = $msgArea.scrollHeight
-  $input.value = ''
+  $msgs.appendChild(msgE);
+  $msgArea.scrollTop = $msgArea.scrollHeight;
+  $input.value = "";
 
-  ws.emit('message', esc(msg))
-})
+  ws.emit("message", esc(msg));
+});
 
-ws.register('peopleOnline', async (data) => {
-  $peopleOnline.innerHTML = base + noise + +data
-})
+ws.register("peopleOnline", async (data) => {
+  $peopleOnline.innerHTML = base + noise + +data;
+});
 
-ws.register('connected', async (data) => {
-  const params = new URLSearchParams(window.location.search)
+ws.register("connected", async (data) => {
+  const params = new URLSearchParams(window.location.search);
   const interests =
     params
-      .get('interests')
-      ?.split(',')
+      .get("interests")
+      ?.split(",")
       .filter((x) => !!x)
-      .map((x) => x.trim()) || []
+      .map((x) => x.trim()) || [];
 
-  let commonInterests = data.at(-1) || ''
-  const first = data.slice(0, -1)
+  let commonInterests = data.at(-1) || "";
+  const first = data.slice(0, -1);
   if (first.length) {
-    commonInterests = `${first.join(', ')} and ${commonInterests}`
+    commonInterests = `${first.join(", ")} and ${commonInterests}`;
   }
 
-  $msgs.innerHTML = ''
-  const status = document.createElement('div')
-  status.className = 'message-status'
-  status.innerHTML = 'You are now chatting with a random stranger'
-  $msgs.appendChild(status)
+  $msgs.innerHTML = "";
+  const status = document.createElement("div");
+  status.className = "message-status";
+  status.innerHTML = "You are now chatting with a random stranger";
+  $msgs.appendChild(status);
   if (commonInterests) {
-    const status = document.createElement('div')
-    status.className = 'message-status'
-    status.innerHTML = `You both like ${esc(commonInterests)}`
-    $msgs.appendChild(status)
+    const status = document.createElement("div");
+    status.className = "message-status";
+    status.innerHTML = `You both like ${esc(commonInterests)}`;
+    $msgs.appendChild(status);
   } else if (interests.length) {
-    const status = document.createElement('div')
-    status.className = 'message-status'
+    const status = document.createElement("div");
+    status.className = "message-status";
     status.innerHTML =
-      "Couldn't find anyone with similar interests, so this stranger is completely random. Try adding more interests!"
-    $msgs.appendChild(status)
+      "Couldn't find anyone with similar interests, so this stranger is completely random. Try adding more interests!";
+    $msgs.appendChild(status);
   }
-  $msgArea.scrollTop = $msgArea.scrollHeight
-  $sendBtn.disabled = false
-  $input.readOnly = false
-})
+  $msgArea.scrollTop = $msgArea.scrollHeight;
+  $sendBtn.disabled = false;
+  $input.readOnly = false;
+});
 
-ws.register('message', async (msg) => {
-  if (!msg) return
+ws.register("message", async (msg) => {
+  if (!msg) return;
 
-  const msgE = document.createElement('div')
-  msgE.className = 'message'
-  msgE.innerHTML = `<span class="strange">Stranger:</span> ${esc(msg)}`
+  const msgE = document.createElement("div");
+  msgE.className = "message";
+  msgE.innerHTML = `<span class="strange">Stranger:</span> ${esc(msg)}`;
 
-  $msgs.appendChild(msgE)
-  $msgArea.scrollTop = $msgArea.scrollHeight
-})
+  $msgs.appendChild(msgE);
+  $msgArea.scrollTop = $msgArea.scrollHeight;
+});
 
-ws.register('typing', async (isTyping) => {
-  $typing.style.display = isTyping ? 'block' : 'none'
-  $msgArea.scrollTop = $msgArea.scrollHeight
-})
+ws.register("typing", async (isTyping) => {
+  $typing.style.display = isTyping ? "block" : "none";
+  $msgArea.scrollTop = $msgArea.scrollHeight;
+});
 
-ws.register('disconnect', async () => {
-  console.log('received disconnect request')
-  disconnectConnection()
+ws.register("disconnect", async () => {
+  console.log("received disconnect request");
+  disconnectConnection();
   // initializeConnection()
-})
+});
 
-configureChat()
-initializeConnection()
+configureChat();
+initializeConnection();
